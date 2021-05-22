af2e1eee51a2232fb8423d9fb677225582b819d9
Félix Luján <felix.lujan@recordrentacar.com>
2021-05-22 11:59:27 +0200

    Create valueObject Size

---
+++ CoverFactory/Manhole/Domain/Size.php
@@ -0,0 +1,25 @@
+<?php
+
+
+namespace CoverFactory\Manhole\Domain;
+
+
+use CoverFactory\Manhole\Domain\Exceptions\InvalidParamsException;
+use CoverFactory\Shared\ValueObjects\StringValueObject;
+
+class Size extends StringValueObject
+{
+    const S = 'S';
+    const M = 'M';
+    const L = 'L';
+    const XL = 'XL';
+
+    const SIZES = [self::S, self::M, self::L, self::XL];
+
+    function validateValue()
+    {
+        if (!in_array($this->value, self::SIZES, true)) {
+            throw new InvalidParamsException("$this->value not valid size. The valid parameters are " . implode(', ', self::SIZES));
+        }
+    }
+}
